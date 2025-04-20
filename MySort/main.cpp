466c4335a1e77dceb0154d8c8ae862c77c16477a
Modin <eom3658@gmail.com>
2025-04-20 16:53:04 +0900

    삽입 정렬 구현

--- MySort/main.cpp
+++ MySort/main.cpp
@@ -5,7 +5,7 @@
 int main()
 {
     std::vector<int> arr = { 5, 2, 9, 1, 5, 6 };
-    sorter::bubble_sort(arr, arr.size());
+    sorter::insertion_sort(arr, arr.size());
 
     for (auto each : arr)
     {
